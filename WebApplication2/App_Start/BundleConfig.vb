2c64e451aeb9c2237b7f5dc3d2e30436fc5ef26e
Samuel Chow <40840173+samuel-cee@users.noreply.github.com>
2019-01-21 12:30:05 -0500

    main1

---
+++ WebApplication2/App_Start/BundleConfig.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+
+Public Class BundleConfig
+    ' For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
+    Public Shared Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+
+    End Sub
+End Class
